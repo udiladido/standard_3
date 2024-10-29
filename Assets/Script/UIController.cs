9cc2eab3d55596e712a08b1771b00e5a10c2e25a
udiladido <89720389+udiladido@users.noreply.github.com>
2024-10-29 14:03:54 +0900

    Commnet : 주석 추가

--- Assets/Script/UIController.cs
+++ Assets/Script/UIController.cs
@@ -2,6 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// 동적으로 할당하는 방법
+// 1.FindObjectOfType<>으로 UI tag 묶어서 찾는다?
+// 2. 싱글톤을 활용해 직접 넣어 참조한다.
+// text
+
 public class UIController : Singleton<UIController>
 {
    
