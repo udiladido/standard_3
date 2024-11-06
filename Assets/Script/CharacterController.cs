8b18a1f808b89c9b99eb1cc10e5db253c4838059
udiladido <89720389+udiladido@users.noreply.github.com>
2024-11-06 12:59:37 +0900

    Init : 4주차 세팅
    에셋 추가 및 폴더 생성

--- Assets/Script/CharacterController.cs
+++ Assets/Script/CharacterController.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
+
+
+    [Header("Movement")]
+    public float moveSpeed;
+    private Vector2 curMovementInput;
+    public float jumptForce;
+    public LayerMask groundLayerMask;
+
     [Header("Look")]
-    public Transform CameraContainer;
+    public Transform cameraContainer;
     public float minXLook;
     public float maxXLook;
     public float minYLook;
     public float maxYLook;
-    private float CamCurXRot;
-    private float CamCurYRot;
+    private float camCurXRot;
+    private float camCurYRot;
     public float lookSensitivity;
- 
+
+    private Vector2 mouseDelta;
+    private Rigidbody rigidbody;
 
     bool canLook = true;
 
@@ -25,6 +35,11 @@ public class CharacterController : MonoBehaviour
 
         Cursor.lockState = CursorLockMode.Locked;
 
+    }
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
     }
 
 
@@ -39,6 +54,32 @@ public class CharacterController : MonoBehaviour
         }
     }
 
+
+    private void LateUpdate()
+    {
+        if (canLook)
+        {
+            CameraLook();
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+
+    private void Move()
+    {
+        Vector3 dir = transform.forward * curMovementInput.y + transform.right * curMovementInput.x;
+        dir *= moveSpeed;
+        dir.y = rigidbody.velocity.y;
+
+        rigidbody.velocity = dir;
+    }
+
+
     public void CallUIEvent()
     { 
     
@@ -46,7 +87,32 @@ public class CharacterController : MonoBehaviour
         
     }
 
+    public void OnLookInput(InputAction.CallbackContext context)
+    {
+        mouseDelta = context.ReadValue<Vector2>();
+    }
 
+
+    void CameraLook()
+    {
+        camCurXRot += mouseDelta.y * lookSensitivity;
+        camCurXRot = Mathf.Clamp(camCurXRot, minXLook, maxXLook);
+        cameraContainer.localEulerAngles = new Vector3(-camCurXRot, 0, 0);
+
+        transform.eulerAngles += new Vector3(0, mouseDelta.x * lookSensitivity, 0);
+    }
+
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            curMovementInput = context.ReadValue<Vector2>();
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            curMovementInput = Vector2.zero;
+        }
+    }
 
 
 }
